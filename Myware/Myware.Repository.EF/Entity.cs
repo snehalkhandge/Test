f243b6a673fad227994b7d652c41096197cccf2f
abstract4713 <mallabhinav@hotmail.com>
2014-07-08 13:13:04 -0400

    Test 2

---
+++ Myware/Myware.Repository.EF/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Myware.Repository.Infrastructure;
+
+namespace Myware.Repository.EF
+{
+    public abstract class Entity : IObjectState
+    {
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
